9c8d815f60075735e305f6355fb194c6e9d4bcec
Hamed0406 <h.adeli.meadeli@gmail.com>
2020-11-27 21:03:27 +0100

    Calculator version1.0 is finished . improvements will be develop in futures (Clear button,Exit button ) .

--- scr/JavaFXCalculator/MainController.java
+++ scr/JavaFXCalculator/MainController.java
@@ -1,6 +1,11 @@
 package JavaFXCalculator;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+import java.awt.*;
 
 /**
  * @author Hamed Adeli,
@@ -8,12 +13,45 @@ import javafx.event.ActionEvent;
  * 11/26/2020
  */
 public class MainController {
+    @FXML
+    private Label result;
+    private long operand1=0;
+    private String operator="";
+    private boolean start=true;
+   private Model model=new Model();
+
+    @FXML
     public void processNumber(ActionEvent event)
     {
-
+if(start)
+{
+    result.setText("");
+    start=false;
+}
+String value=((Button)event.getSource()).getText();//Convert button text to string
+        result.setText(result.getText()+value);
     }
+    @FXML
     public void processOperators(ActionEvent event)
     {
+        String value=((Button)event.getSource()).getText();//Convert button text to string
+if(!value.equals("="))//Negative scenario
+{
+    if(!operator.isEmpty())
+        return;
+    operator=value;
+    operand1=Long.parseLong(result.getText());
+    result.setText("");
 
+}else
+{
+    if(operator.isEmpty())
+        return;
+    long operand2=Long.parseLong(result.getText());
+    float outPut= model.calulate(operand1,operand2,operator);
+    result.setText(String.valueOf(outPut));
+    operator="";
+    start=true;
+}
     }
 }//end of MainController class.
--- scr/JavaFXCalculator/Model.java
+++ scr/JavaFXCalculator/Model.java
@@ -11,6 +11,20 @@ public class Model {
     public float calulate(long operand1,long operand2,String operator)
     {//TODO
         //Creat method for calculation .
-        return 0;
+        switch (operator){
+            case"+":
+              return   operand1+operand2;
+            case"-":
+                return operand1-operand2;
+            case"*":
+                return operand1*operand2;
+            case "/":
+                if(operand2==0)
+                    return 0;
+                return operand1/operand2;
+            default:
+                return 0;//For any symbol is not match to any case return  0.
+
+        }//End of switch.
     }
 }//End of Model class
