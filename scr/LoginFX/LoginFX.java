0ceaf1b24e513834d6083d73892a0a5bfef02ec5
Hamed0406 <h.adeli.meadeli@gmail.com>
2020-11-27 21:10:16 +0100

    Skeleton for simple LoginFX with two scene .

---
+++ scr/LoginFX/LoginFX.java
@@ -0,0 +1,21 @@
+package LoginFX;
+/**
+ * @author Hamed Adeli,
+ * h.adeli.meadeli@gmail.com
+ * 11/27/2020
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class LoginFX extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
---
+++ scr/LoginFX/MainController.java
@@ -0,0 +1,9 @@
+package LoginFX;
+
+/**
+ * @author Hamed Adeli,
+ * h.adeli.meadeli@gmail.com
+ * 11/27/2020
+ */
+public class MainController {
+}
