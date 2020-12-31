458fb387c21e358433f456e9513c279e5ceaac39
Hamed0406 <h.adeli.meadeli@gmail.com>
2020-12-31 11:09:06 +0100

    add Doc

---
+++ scr/Random/TimerTest.java
@@ -0,0 +1,35 @@
+package Random;
+
+/**
+ * @author Hamed Adeli,
+ * h.adeli.meadeli@gmail.com
+ * 11/28/2020
+ */
+import java.util.Timer;
+import java.util.TimerTask;
+
+
+
+    public class TimerTest {
+        public static void main(String[] args) {
+            System.out.println("Starting up...");
+
+            //one-time use timer: prints stuff after 10s
+            Timer myTimer = new Timer();
+            myTimer.schedule(new TimerTask(){
+                @Override
+                public void run() {
+                    System.out.println("hello from timer");
+                }
+            }, 10000);
+
+            //repeating timer: prints stuff every 10s
+            Timer myRepeatingTimer = new Timer();
+            myRepeatingTimer.scheduleAtFixedRate(new TimerTask(){
+                @Override
+                public void run(){
+                    System.out.println("hello from repeating timer");
+                }
+            }, 0, 1000);
+        }
+    }
